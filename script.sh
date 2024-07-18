d85cf0a869a3b254eddd57091e8b17a518998547
Oscar-de-Sevelinges-Wavestone <oscar.desevelinges@wavestone.com>
2024-07-18 10:52:18 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Création du script az-logger.sh
-echo "#!/bin/bash" >> /home/azureuser/az-logger.sh
-echo "commande="$*"" >> /home/azureuser/az-logger.sh
-echo "output=$(az "$@" 2>&1)" >> /home/azureuser/az-logger.sh
-echo "if [[ $output == *"Code: AuthorizationFailed"*  ]]; then" >> /home/azureuser/az-logger.sh
-echo "        echo "$commande" | logger -t az" >> /home/azureuser/az-logger.sh
-echo "        echo "$output" | logger -t az" >> /home/azureuser/az-logger.sh
-echo "fi" >> /home/azureuser/az-logger.sh
-echo "echo "$output"" >> /home/azureuser/az-logger.sh
-chmod +x /home/azureuser/az-logger.sh
+echo "#!/bin/bash" >> /etc/az-logger.sh
+echo "commande="$*"" >> /etc/az-logger.sh
+echo "output=$(az "$@" 2>&1)" >> /etc/az-logger.sh
+echo "if [[ $output == *"Code: AuthorizationFailed"*  ]]; then" >> /etc/az-logger.sh
+echo "        echo "$commande" | logger -t az" >> /etc/az-logger.sh
+echo "        echo "$output" | logger -t az" >> /etc/az-logger.sh
+echo "fi" >> /etc/az-logger.sh
+echo "echo "$output"" >> /etc/az-logger.sh
+chmod +x /etc/az-logger.sh
 
 # Modification du fichier /etc/bash.bashrc
-echo "alias az="/home/azureuser/az-logger.sh"" >> /etc/bash.bashrc
+echo "alias az="/etc/az-logger.sh"" >> /etc/bash.bashrc
 source /etc/bash.bashrc
