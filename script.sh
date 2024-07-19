19f2cd86a38dac79d7aa2055d47e1ed526fee835
Oscar-de-Sevelinges-Wavestone <oscar.desevelinges@wavestone.com>
2024-07-19 15:39:17 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,5 +29,5 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 echo "export HISTCONTROL=ignoredups:erasedups" >> /etc/bash.bashrc
 echo "shopt -s histappend" >> /etc/bash.bashrc
 echo "export PROMPT_COMMAND=\"history -a; \$PROMPT_COMMAND\"" >> /etc/bash.bashrc
-# echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(who am i) [\$\$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/bash.bashrc
+echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(who am i) [\$\$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/bash.bashrc
 source /etc/bash.bashrc
