b60df9ef02c60767210298219aa0fdc41a8521bf
Oscar-de-Sevelinges-Wavestone <oscar.desevelinges@wavestone.com>
2024-07-19 15:38:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,8 +26,8 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Modification du fichier /etc/bash.bashrc
 # echo "alias ssh=\"/etc/ssh-logger.sh\"" >> /etc/bash.bashrc
-# echo "export HISTCONTROL=ignoredups:erasedups" >> /etc/bash.bashrc
-# echo "shopt -s histappend " >> /etc/bash.bashrc
-# echo "export PROMPT_COMMAND=\"history -a; \$PROMPT_COMMAND\"" >> /etc/bash.bashrc
+echo "export HISTCONTROL=ignoredups:erasedups" >> /etc/bash.bashrc
+echo "shopt -s histappend" >> /etc/bash.bashrc
+echo "export PROMPT_COMMAND=\"history -a; \$PROMPT_COMMAND\"" >> /etc/bash.bashrc
 # echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(who am i) [\$\$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/bash.bashrc
-# source /etc/bash.bashrc
+source /etc/bash.bashrc
